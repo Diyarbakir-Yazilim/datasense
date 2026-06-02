f9b5a6651940ef1a6ed021a6adcd0949bf747250
ismail topalan <ismailtopalan@gmail.com>
2026-06-02 15:55:55 +0300

    nitialization code

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -1,13 +1,2 @@
 # DataSense Backend - Celery
 # TODO: Celery/Redis asenkron kuyruk yönetimi eklenecek
-from celery import Celery
-from backend.app.core.config import settings
-
-celery_app = Celery(
-    "datasense_tasks",
-    broker=settings.CELERY_BROKER_URL,
-    backend=settings.CELERY_RESULT_BACKEND
-)
-
-# Celery görevlerinin (tasks) nerede olduğunu otomatik taraması için yönlendirme yapıyoruz
-celery_app.autodiscover_tasks(["backend.app.tasks"])
