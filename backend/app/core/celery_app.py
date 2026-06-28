70d7ee8b369468d93a3fa0fc7ac4a17c0261198a
amedgl <151673707+amedgl@users.noreply.github.com.>
2026-06-28 17:35:22 +0300

    Fix celery redis configuration and backend async result

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -6,7 +6,8 @@ from app.core.config import settings
 celery_app = Celery(
     "worker",
     broker=settings.REDIS_URL,
-    backend=settings.REDIS_URL
+    backend=settings.REDIS_URL,
+    include=["app.worker.tasks"]
 )
 
 celery_app.conf.task_routes = {
